d6743ae82c38e74081097994d14c591efde579c6
Md. Naimul Islam <162487286+naimul1322@users.noreply.github.com>
2026-03-11 10:45:49 +0600

    Day2

---
+++ biodata.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+   /* cout<<"This is my first program C++\n"<<endl;
+    cout<<"i love you prgramming\n"<<endl;*/
+    cout<<"i love you prgramming\n"<<endl;
+
+
+    return 0;
+}
+
---
+++ userinput.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    while(1)
+    {
+        int x,y,z;
+    cout<<"Enter the X:\n";
+    cin>>x;
+    cout<<"Enter the Y:\n";
+    cin>>y;
+ //   z=x+y;
+    cout<<"Z is: "<<x+y <<endl;
+    cout<<"Z is: "<<x-y<<endl;
+    cout<<"Z is: "<<x*y<<endl;
+    cout<<"Z is: "<<x/y<<endl;
+    cout<<"Z is: "<<x%y<<endl;
+    }
+}
